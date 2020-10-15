41f6670d91c718e351de924440e400687b13833d
Jessica Farr <jesfar@victorchang.edu.au>
2020-10-15 11:24:45 +1100

    successfully identifies depol activation time for spontaneous datasets

--- analyse_MEA_signals.m
+++ analyse_MEA_signals.m
@@ -1,27 +1,168 @@
-function analyse_MEA_signals(input_file, window, beat_detection_thresh, beat_by_beat)
+function analyse_MEA_signals(input_file, beat_by_beat, spon_paced, well_thresholding, UI)
 % analyse_MEA_signals(fullfile('data', '20200526_70-1915_empty(000).raw'), 1, 300*E-3)
 
-    plot_prompt = 'What would you like to name the directory that stores the plots of the data?\n';
-    plot_dir = prompt_user(plot_prompt, 'dir', 'data');
+    tic;
+    %plot_prompt = 'What would you like to name the directory that stores the plots of the data?\n';
+    %plot_dir = prompt_user(plot_prompt, 'dir', 'data');
     
     FileData = AxisFile(input_file);
     AllDataRaw = FileData.DataSets.LoadData;
+    
+    %Display the user with the plots of each 
+
+
+    if strcmpi(well_thresholding, 'on')
+
+        shape_data = size(AllDataRaw);
+        num_well_rows = shape_data(1);
+        num_well_cols = shape_data(2);
+        num_electrode_rows = shape_data(3);
+        num_electrode_cols = shape_data(4);
         
-    %Dummy test ensuring a function can access the other files
+        num_well_rows = 3;
+        num_well_cols = 1;
+        
+        total_wells = num_well_rows*num_well_cols;
+        well_thresholds = double.empty(total_wells, 0);
+
+        well_count = 0;
+        well_dictionary = ['A', 'B', 'C', 'D', 'E', 'F'];
+        for w_r = 1:num_well_rows
+            for w_c = 1:num_well_cols
+                well_count = well_count + 1;
+                wellID = strcat(well_dictionary(w_r), '0', string(w_c));
+                disp(strcat('Displaying well: ', {' '}, wellID))
+                sub_plot_count = 1;
+                %for e_r = 1:num_electrode_rows
+                
+                waveform = 0;
+                for e_r = num_electrode_rows:-1:1
+                    for e_c = 1:num_electrode_cols
+                    %for e_c = num_electrode_cols:-1:1
+                        WellRawData = AllDataRaw{w_r, w_c, e_r, e_c};
+                        if strcmp(class(WellRawData),'Waveform')
+                            %if ~empty(WellRawData)
+                            [time, data] = WellRawData.GetTimeVoltageVector;
+                            
+                            if waveform == 0
+                                figure()
+                                plot_cutoff_end_indx = floor(size(time)*0.15);
+                                
+                            end
+                               
+                            waveform = 1;
+                            electrode_id = strcat(wellID, {' '}, string(e_c), {' '}, string(e_r));
+                            %disp(electrode_id)
+                            
+
+                             %fig = figure();
+                             %subplot(num_electrode_rows, num_electrode_cols, sub_plot_count);
+                             %set(fig ,'Visible', 'off');
+                             plot(time(1:plot_cutoff_end_indx), data(1:plot_cutoff_end_indx));
+                             xlabel('Time');
+                             ylabel('Voltage');
+                             hold on;
+                             %title(electrode_id);
+                             %print(fullfile(plot_dir, electrode_id), '-dbitmap', '-r0');
+
+                             sub_plot_count = sub_plot_count + 1; 
+
+                        end
+                    end
+                end
+                if waveform == 1
+                    title(wellID);
+                    input_string = strcat('Enter the beat detection threshold for well', {' '}, wellID, ': \n');
+                    beat_detection_thresh = input(input_string);
+                    %beat_detection_thresh = 4E-4;
+                    well_thresholds(well_count) = beat_detection_thresh;
+                    hold off;
+                end
+            end
+        end
+
+
+        well_count = 0;
+        for w_r = 1:num_well_rows
+            for w_c = 1:num_well_cols
+                well_count = well_count + 1;
+                wellID = strcat(well_dictionary(w_r), '0', string(w_c));
+
+                for e_r = num_electrode_rows:-1:1
+                    for e_c = 1:num_electrode_cols
+                        WellRawData = AllDataRaw{w_r, w_c, e_r, e_c};
+                        if strcmp(class(WellRawData),'Waveform')
+                        %if ~empty(WellRawData)
+                            [time, data] = WellRawData.GetTimeVoltageVector;
+
+                            extract_beats(time, data, well_thresholds(well_count), spon_paced);                             
+
+                        end
+                    end
+                end
+            end
+        end
+    else
+        
+        view_sample_well = input('Would you like to view an example well to choose the beat detection threshold?(yes/no):\n', 's');
+        if strcmpi(view_sample_well, 'yes')
+            [time, data] = AllDataRaw{6, 1, 1, 1}.GetTimeVoltageVector;
+            figure();
+            plot(time, data);
+            %plot(data, time);
+            xlabel('Time (secs)')
+            ylabel('Voltage (V)');
+            title('MEA waveform')
+        end
+        
+        beat_detection_thresh = input('Enter the beat detection threshold: \n');
+        shape_data = size(AllDataRaw);
+        num_well_rows = shape_data(1);
+        num_well_cols = shape_data(2);
+        num_electrode_rows = shape_data(3);
+        num_electrode_cols = shape_data(4);
+
+        count = 1;
+        well_dictionary = ['A', 'B', 'C', 'D', 'E', 'F'];
+        for w_r = 1:num_well_rows
+            for w_c = 1:num_well_cols
+                wellID = strcat(well_dictionary(w_r), '0', string(w_c));
+                disp(wellID);
+                %for e_r = 1:num_electrode_rows
+                for e_r = num_electrode_rows:-1:1
+                    for e_c = 1:num_electrode_cols
+                    %for e_c = num_electrode_cols:-1:1
+                    WellRawData = AllDataRaw{w_r, w_c, e_r, e_c};
+                        if strcmp(class(WellRawData),'Waveform')
+                            %if ~empty(WellRawData)
+                            electrode_id = strcat(wellID, {' '}, string(e_c), {' '}, string(e_r));
+                            disp(electrode_id)
+                            [time, data] = WellRawData.GetTimeVoltageVector;
+                            extract_beats(time, data, beat_detection_thresh, spon_paced);  
+                            count = count + 1;
+                        end
+                    end
+                end
+            end
+        end
+        
+    end
+
     
-    
+    %{
     [time, data] = AllDataRaw{6, 1, 1, 1}.GetTimeVoltageVector;
-    %figure(1);
-    %plot(time, data);
+    figure();
+    plot(time, data);
     %plot(data, time);
-    %xlabel('Time (secs)')
-    %ylabel('Voltage (V)');
-    %title('Continuous MEA waveform')
+    xlabel('Time (secs)')
+    ylabel('Voltage (V)');
+    title('MEA waveform')
+    
+    beat_detection_thresh = input('Enter the beat detection threshold: \n');
     
     %disp(size(AllDataRaw))
-    extract_beats(time, data, window, beat_detection_thresh)
-          
-    
+    extract_beats(time, data, beat_detection_thresh, spon_paced)
+    %}  
     
     %This bit of code is to be used to the high throughput data analysis
     
@@ -63,84 +204,73 @@ function analyse_MEA_signals(input_file, window, beat_detection_thresh, beat_by_
         end
     end
     %}
+    elapsed_time_secs = toc;
+    elapsed_time_mins = elapsed_time_secs/60;
+    elapsed_time_hours = elapsed_time_mins/60;
+    disp(['Total run time was ' num2str(floor(elapsed_time_hours)) ' hours and ' num2str(mod(floor(elapsed_time_mins), 60)) ' minutes.']);
 end
 
-function [beat_period, fpd] = extract_beats(time, data, window, bdt)
+function extract_beats(time, data, bdt, spon_paced)
     total_duration = time(end);
-    
+    disp(total_duration)
     prev_beat_indx = 1;
     beat_indx = 1;
-    max_beat_period = 1.5;  %seconds
-    min_beat_period = 0.5;  %seconds
+    
+    max_beat_period = 17;  %seconds
+    min_beat_period = 0.7;  %seconds
     post_spike_hold_off = 0.2;   %seconds
+    window = 1.1;
     
     
     %% New approach: while(1) until final element of the segment being analysed is past the duration
     %%               Find the first beat in the window frame
     count = 0;
-    for t = 0:window:total_duration
+    t = 0;
+    %for t = 0:window:total_duration
+    while(1)
        %disp(t+window)
        %Use the beat detection threshold to determine the regions that need
        %to be analysed
        
+       if (time(prev_beat_indx)+window) > total_duration
+           break;
+       end           
+           
        %Take segments of data from each window to search for the next beat 
        if beat_indx == 1
        %if t == 0
-          wind_indx = find(time >= 0 & time <= window);  
+           wind_indx = find(time >= 0 & time <= total_duration);  
+          %wind_indx = find(time >= 0 & time <= window);  
           %wind_indx = find(time >= t & time <= t+window);
        else
-          wind_indx = find(time >= time(prev_beat_indx) & time <= (time(prev_beat_indx)+window)); 
+          wind_indx = find(time >= time(prev_beat_indx) & time <= total_duration); 
+          %wind_indx = find(time >= time(prev_beat_indx) & time <= (time(prev_beat_indx)+window)); 
           %wind_indx = find(time >= t & time <= t+window);
        end
        
        t_ime = time(wind_indx);
        d_ata = data(wind_indx);
-       
-       %{
-       disp('start search window')
-       disp(wind_indx(1))
-       disp('end search window')
-       disp(wind_indx(end))
-       
-       
-       figure()
-       plot(t_ime, d_ata);
-       title('window to be analysed')
-       hold off;
-       %} 
-       %disp(max(d_ata))
-       try
-           
+
+       try 
            post_spike_hold_off_time = t_ime(1)+post_spike_hold_off;
            pshot_indx = find(t_ime >= post_spike_hold_off_time);
            pshot_indx_offset = pshot_indx(1);
-           
-           %{
-           disp('start post hold off region');
-           disp(pshot_indx(1))
-           
-           disp('end post hold off region');
-           disp(pshot_indx(end))
-           %}
-        
-           
+                      
            beat_indx = find(d_ata(pshot_indx) >= bdt);
-           %beat_indx = find(d_ata >= bdt);
            beat_indx = beat_indx(1)+wind_indx(1)-1+pshot_indx_offset;
-           %{
-           disp('beat')
-           disp(beat_indx)
-           disp('prev')
-           disp(prev_beat_indx)
-           %}
-       
+
        catch
-           prev_beat_indx = beat_indx;
+           %Update the beat detection threshold
            bdt = bdt*0.1;
+           
+           % If fails on the first iteration, set the beat index to be back to the first point
            if prev_beat_indx == 1
                beat_indx = 1;
+           else
+               beat_indx = prev_beat_indx;
            end
            disp('fail beat detection, continue')
+           t = t - window;
            continue;
        end
        
@@ -148,7 +278,6 @@ function [beat_period, fpd] = extract_beats(time, data, window, bdt)
        
        beat_time = time(prev_beat_indx:beat_indx);
        beat_data = data(prev_beat_indx:beat_indx);
-       %disp(max(beat_data)) 
        
        %% Trim and then scale and set back to time zero for each beat
        %% Start small degrees of freedom [1-100], polyfits keep trying and do a survey of error plots, do the optimisation curve thing minimised error analysis 
@@ -157,61 +286,50 @@ function [beat_period, fpd] = extract_beats(time, data, window, bdt)
        try
           beat_period = beat_time(end) - beat_time(1);
        catch
-          prev_beat_indx = beat_indx;
+          beat_indx = prev_beat_indx;
           continue;
        end
        
+       disp(strcat('Beat period = ', num2str(beat_period)));
+       disp(strcat('Beat start time = ', num2str(beat_time(1))));
+       disp(strcat('Beat end time = ', num2str(beat_time(end))));
+       
        if beat_period > max_beat_period
        %Check there is only one beat period.
-          bdt = bdt*0.1;
           disp('bdt has been reduced due to beat period being too long')
           disp(bdt)
           
-          if prev_beat_indx == 1
-               beat_indx = 1;
+          if prev_beat_indx ~= 1
+              bdt = bdt/2;
+              window = window*0.8;
+              beat_indx = prev_beat_indx;
           else
               prev_beat_indx = beat_indx;
-           end
+          end
+          t = t - window;
           continue;
        end
        
        if beat_period < min_beat_period
-          bdt = bdt*10;
           disp('bdt has been increased due to beat period being too short')
           disp(bdt)
-          disp(prev_beat_indx)
-          if prev_beat_indx == 1
-               beat_indx = 1;
+          if prev_beat_indx ~= 1
+              beat_indx = prev_beat_indx;
+              bdt = bdt*5;
+              window = window*1.5;
           else
               prev_beat_indx = beat_indx;
           end
+          t = t - window;
           continue;
        end
-       
-       
-       
-       %Now fit the data and find the peak of the t-wave then use this to
-       %find FPD
-       %{
-       min_signal = min(beat_data);
-       min_indx = find(beat_data == min_signal);
-       beat_data = beat_data(min_indx:end);
-       beat_time = beat_time(min_indx:end);
-       %}
-       
-       %beat_time = beat_time - beat_time(1);
-       %beat_data = beat_data * 1e4;
-       %[poly_f, delta] = polyfit(beat_time, beat_data, 15);
-       %fit = polyval(poly_f, beat_time);
-       %disp(size(fit))
-       %disp(size(beat_time))
-       %disp(delta.normr);
-       
+            
+       depolarisation_complex_analysis(beat_time, beat_data, post_spike_hold_off)
        
        threshold_data = zeros(size(beat_time));
        threshold_data(:) = bdt;
        
-       
+       %{
        figure();
        plot(beat_time, beat_data);
        hold on;
@@ -219,32 +337,60 @@ function [beat_period, fpd] = extract_beats(time, data, window, bdt)
        xlabel('Time (secs)')
        ylabel('Voltage (V)')
        title('Extracted Beat');
+       
        %hold on;
        %plot(beat_time, fit);
-       pause(5);
+       %pause(5);
        %close(gcf)
        hold off;
-       
-       %{
-       figure();
-       plot(time(wind_indx), data(wind_indx));
-       hold on;
-       plot(t_ime(beat_indx), d_ata(beat_indx), 'ro');
-       hold off;
-       pause(10);
-       close(gcf)
        %}
+
        
        prev_beat_indx = beat_indx;
        count = count + 1;
+       t = t + window;
        %pause(5);
        %close('all');
     end
-    disp(count);
+    %disp(count);
 
-    % 0.0005
 end
 
+function depolarisation_complex_analysis(time, data, post_spike_hold_off)
+    post_spike_hold_off_time = time(1)+post_spike_hold_off;
+    pshot_indx = find(time >= post_spike_hold_off_time);
+    pshot_indx_offset = pshot_indx(1);
+    
+    depol_complex_time = time(1:pshot_indx_offset);
+    depol_complex_data = data(1:pshot_indx_offset);
+    depol_complex_data_derivative = gradient(depol_complex_data);
+    %depol_complex_data_derivative = gradient(depol_complex_data_derivative);
+    
+    max_slope_point = find(depol_complex_data_derivative == min(depol_complex_data_derivative));
+    
+    %% To further refine the region to isolate the depol complex, perfrom std analysis
+    
+    
+    figure()
+    hold on;
+    plot(depol_complex_time, depol_complex_data);
+    plot(depol_complex_time, depol_complex_data_derivative);
+    plot(depol_complex_time(max_slope_point(1)), depol_complex_data(max_slope_point(1)), 'ro')
+    xlabel('Time (secs)')
+    ylabel('Voltage (V)')
+    title('Extracted Beat');
+    pause(5);
+    
+    
+    
+    
+
+end
+
+function t_wave_complex_analysis(time, data)
+
+
+end
 
 function extract_beat_linear_fit(time, data, window)
     total_duration = 100;
